9871841e7bf916e5c2595d63869348078368af28
Arthem O <oarthemlive@gmail.com>
2023-01-06 16:22:59 +0300

    Added leftsidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div :class="{'dark':darkMode}">
+    <div class="dark:bg-dim-900 bg-white">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!--    left sidebar-->
+
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft/>
+            </div>
+          </div>
+
+          <!--        main -->
+
+          <!--        right side-->
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+
+const darkMode = ref(false)
+</script>
